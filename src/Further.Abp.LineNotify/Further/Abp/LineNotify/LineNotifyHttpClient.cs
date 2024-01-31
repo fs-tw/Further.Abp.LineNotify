af8f9c1fa574365ae6b9906c2ff2cf858734efcc
ggoch <loyuqingan@gmail.com>
2024-01-31 16:17:27 +0800

    refactor: line-notyfy

--- src/Further.Abp.LineNotify/Further/Abp/LineNotify/LineNotifyHttpClient.cs
+++ src/Further.Abp.LineNotify/Further/Abp/LineNotify/LineNotifyHttpClient.cs
@@ -1,121 +1,18 @@
-﻿using HttpTracer;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
-using RestSharp;
-using System.Threading.Tasks;
+﻿using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
 
 namespace Further.Abp.LineNotify
 {
-    public class LoggerWraper<T> : HttpTracer.Logger.ILogger
+    public class LineNotifyHttpClient : LineNotifyHttpClientBase
     {
-        private readonly ILogger<T> logger;
-
-        public LoggerWraper(
-            ILogger<T> logger)
-        {
-            this.logger=logger;
-        }
-        public void Log(string message)
-        {
-            this.logger.LogInformation(message);
-        }
-    }
-    public class LineNotifyHttpClient : ILineNotifyHttpClient, Volo.Abp.DependencyInjection.ITransientDependency
-    {
-        private RestClient notifyBotClient;
-        private RestClient notifyApiClient;
-        private readonly ServiceDefinitionOptions serviceDefinitionOptions;
-
         public LineNotifyHttpClient(
-            IOptions<ServiceDefinitionOptions> serviceDefinitionOptions,
-            ILogger<LineNotifyHttpClient> logger
-            )
+            IOptions<ServiceDefinitionOptions> options,
+            IHttpClientFactory httpClientFactory)
+            : base(options, httpClientFactory)
         {
-
-            this.serviceDefinitionOptions = serviceDefinitionOptions.Value;
-
-            this.notifyBotClient=new RestClient(createRestClientOptions(this.serviceDefinitionOptions.NotifyBotUrl));
-
-            this.notifyApiClient=new RestClient(createRestClientOptions(this.serviceDefinitionOptions.NotifyApiUrl));
-
-            RestClientOptions createRestClientOptions(string url)
-            {
-                var result = new RestClientOptions(url);
-
-                if (this.serviceDefinitionOptions.EnableLogger)
-                {
-                    var loggerWraper = new LoggerWraper<LineNotifyHttpClient>(logger);
-                    result.ConfigureMessageHandler = handler => new HttpTracerHandler(handler, loggerWraper, HttpMessageParts.All);
-                }
-
-                return result;
-            }
-        }
-        protected RestRequest CreateRequest(string resource, string accessToken = null)
-        {
-            var request = new RestRequest(resource);
-
-            if (!string.IsNullOrEmpty(accessToken))
-            {
-                request.AddParameter("Authorization", $"Bearer {accessToken}", ParameterType.HttpHeader);
-            }
-
-            return request;
-        }
-
-        public Task<string> AuthorizeUrlAsync(string state)
-        {
-            var request = this.CreateRequest("authorize");
-
-            //var state = $"name={name}&provider-name={providerName}&provider-key={providerKey}";
-
-            request.AddQueryParameter("response_type", "code");
-
-            request.AddQueryParameter("client_id", serviceDefinitionOptions.ClientId);
-
-            request.AddQueryParameter("redirect_uri", serviceDefinitionOptions.RedirectUrl);
-
-            request.AddQueryParameter("scope", "notify");
-
-            request.AddQueryParameter("state", state);
-
-            return Task.FromResult(notifyBotClient.BuildUri(request).ToString());
-        }
-
-        public async Task<GetTokenResult> TokenAsync(string code)
-        {
-            var request = this.CreateRequest("token");
-
-            var requestObject = new GetToken()
-            {
-                client_id = serviceDefinitionOptions.ClientId,
-
-                client_secret = serviceDefinitionOptions.ClientSecret,
-
-                grant_type = "authorization_code",
-
-                code = code,
-
-                redirect_uri = serviceDefinitionOptions.RedirectUrl
-            };
-
-            request.AddObject(requestObject);
-
-            return await notifyBotClient.PostAsync<GetTokenResult>(request);
-        }
-
-        public async Task NotifyAsync(string accessToken, string message)
-        {
-            var request = this.CreateRequest("notify", accessToken);
-
-            var requestObject = new Notify()
-            {
-                message = message
-            };
-
-            request.AddObject(requestObject);
-
-            await notifyApiClient.PostAsync<NotifyResult>(request);
         }
     }
 }
---
+++ src/Further.Abp.LineNotify/Further/Abp/LineNotify/LineNotifyLoggerHandler.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace Further.Abp.LineNotify
+{
+    public class LineNotifyLoggerHandler: DelegatingHandler
+    {
+        private readonly ILogger<LineNotifyLoggerHandler> logger;
+        private readonly IOptions<ServiceDefinitionOptions> options;
+
+        public LineNotifyLoggerHandler(
+            HttpClientHandler innerHandler,
+            ILogger<LineNotifyLoggerHandler> logger,
+            IOptions<ServiceDefinitionOptions> options)
+            :base(innerHandler)
+        {
+            this.logger = logger;
+            this.options = options;
+        }
+        protected override async System.Threading.Tasks.Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, System.Threading.CancellationToken cancellationToken)
+        {
+            if (options.Value.EnableLogger)
+            {
+                this.logger.LogInformation($"準備發送訊息至Line Notify， RequestContent: {request.Content}");
+            }
+
+            var response = await base.SendAsync(request, cancellationToken);
+
+            if (options.Value.EnableLogger)
+            {
+                var message = await response.Content.ReadAsStringAsync();
+                
+
+                if (response.IsSuccessStatusCode)
+                {
+                    this.logger.LogInformation($"發送訊息至Line Notify成功， ResponseContent: {message}");
+                }
+                else
+                {
+                    this.logger.LogError($"發送訊息至Line Notify失敗， ResponseContent: {message}");
+                }
+            }
+            return response;
+        }
+    }
+}
